4cb6b6e1872bcb552c03c30c89cbed8c55cf4794
Nitesh Kumar Singh <niteshkumar.singh@infoorigin.com>
2025-07-01 21:15:08 +0530

    implement publisher

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { WinstonLogger } from './config/logger-config';
 import { requestContextStore } from './shared/request/request-middleware';
 import { AppConfigs } from './config/app-config';
+import { RedisService } from './config/cache/redis.service';
 
 @Injectable()
 export class AppService {
   private readonly logger = new WinstonLogger(AppService.name);
+  constructor(private readonly redisService:RedisService){}
 
-  getHello() {
+  getHello(data:any) {
     let id = requestContextStore.getStore()?.get(AppConfigs.request_id)
-
+    console.log(data)
+this.redisService.publish('test',data)
 
 
     return { data: id }
